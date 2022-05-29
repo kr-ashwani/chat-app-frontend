154552211bcbf55a0b37276a4549197d3c09830d
Ashwani Kumar <ashwanikr160@gmail.com>
2022-05-29 09:45:39 +0530

    ec2 prod

--- src/utils/getdateString.js
+++ src/utils/getdateString.js
@@ -8,9 +8,21 @@ const getDateString = (givenTimestamp) => {
     (currentTimestamp - givenTimestamp.getTime()) / (24 * 60 * 60 * 1000);
   let dateString = null;
 
-  if (daysDiff <= 1) dateString = 'today';
-  else if (daysDiff <= 2) dateString = 'yesterday';
-  else if (daysDiff <= 7) dateString = dateFormat(givenTimestamp, 'dddd');
+  if (daysDiff <= 1)
+    dateString =
+      givenTimestamp.getDay() - currentTimestamp.getDay() === 0
+        ? 'today'
+        : 'yesterday';
+  else if (daysDiff <= 2)
+    dateString =
+      givenTimestamp.getDay() - currentTimestamp.getDay() === 1
+        ? 'yesterday'
+        : dateFormat(givenTimestamp, 'dddd');
+  else if (daysDiff <= 7)
+    dateString =
+      givenTimestamp.getDay() - currentTimestamp.getDay() <= 7
+        ? dateFormat(givenTimestamp, 'dddd')
+        : dateFormat(givenTimestamp, 'mm/dd/yyyy');
   else dateString = dateFormat(givenTimestamp, 'mm/dd/yyyy');
 
   return dateString;
